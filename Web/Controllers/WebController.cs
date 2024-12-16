773380d31b57449e5dc24132ac493da91f208d6e
sahoon1 <siblingszahid@gmail.com>
2024-12-16 20:32:38 +0500

    work

---
+++ Web/Controllers/WebController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class WebController : Controller
+    {
+        // GET: Web
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult service()
+        {
+            return View();
+        }
+        public ActionResult portfolio()
+        {
+            return View();
+        }
+        public ActionResult starter()
+        {
+            return View();
+        }
+    }
+}
